461e85d3557be739f66549c90efd174a13f38e12
Jeffrey Michel <jemichel@yahoo.com>
2016-09-03 14:23:13 -0400

    Use synchronous step definitions and string matching

--- features/step_definitions/my_step_definitions.js
+++ features/step_definitions/my_step_definitions.js
@@ -1,23 +1,13 @@
 // features/step_definitions/my_step_definitions.js
 
 module.exports = function () {
-  this.Given(/^I am on the Cucumber.js GitHub repository$/, function (callback) {
-    // Express the regexp above with the code you wish you had.
-    // `this` is set to a World instance.
-    callback();
+  this.Given('I am on the Cucumber.js GitHub repository', function () {
   });
 
-  this.When(/^I go to the README file$/, function (callback) {
-    // Express the regexp above with the code you wish you had. Call callback() at the end
-    // of the step, or callback(null, 'pending') if the step is not yet implemented:
-
-    //callback(null, 'pending');
-    callback();
+  this.When('I go to the README file', function () {
   });
 
-  this.Then(/^I should see "(.*)" as the page title$/, function (title, callback) {
-    // matching groups are passed as parameters to the step definition
-
-    callback();
+  this.Then('I should see "$title" as the page title', function (title) {
+    console.log(title);
   });
 };
